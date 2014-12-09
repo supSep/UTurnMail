42f5e3ad3f788762f2774f928330bb4c0101b8a5
Sep Taheri <sept@uvic.ca>
2014-12-08 22:48:20 -0800

    script

--- uturnmail.py
+++ uturnmail.py
@@ -1,3 +1,6 @@
+
+import email, boto.sqs
+
 # Read from STDIN into array of lines.
 email_input = sys.stdin.readlines()
 
@@ -8,3 +11,15 @@ msg = None
 for msg_line in email_input:
    parser.feed(msg_line)
 msg = parser.close()
+
+from pprint import pprint
+logFile=open('/mnt/spool/uturnmail/script_logs/uno.txt', 'w')
+pprint (vars(msg),logFile)
+
+#Will implement save email to a file first to examine it
+
+#AMAZON SQS
+#conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ465WWFWWSIUEKIQ', aws_secret_access_key='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')
+
+#Get the queue
+#queue = conn.get_queue('myqueue')
