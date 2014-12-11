6f6d03cc70209499d196b6dddc4a1a8c8aea2b01
Sep <Sept@uvic.ca>
2014-12-11 10:22:30 -0800

    Updated python script

--- uturnmail.py
+++ uturnmail.py
@@ -1,25 +1,23 @@
+import email, boto.sqs, sys, time, smtplib
 
-import email, boto.sqs
-
-# Read from STDIN into array of lines.
-email_input = sys.stdin.readlines()
-
-# email.FeedParser.feed() expects to receive lines one at a time
-# msg holds the complete email Message object
-parser = email.FeedParser.FeedParser()
-msg = None
-for msg_line in email_input:
-   parser.feed(msg_line)
-msg = parser.close()
+msg = email.message_from_file(sys.stdin)
 
 from pprint import pprint
 logFile=open('/mnt/spool/uturnmail/script_logs/uno.txt', 'w')
-pprint (vars(msg),logFile)
+pprint(vars(msg),logFile)
+pprint("We made it!", logFile)
+pprint(time.strftime("%c"), logFile)
 
 #Will implement save email to a file first to examine it
 
 #AMAZON SQS
-#conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ465WWFWWSIUEKIQ', aws_secret_access_key='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')
+#conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ465WWFWWSIUEKIQ', aws_secret_access_key='kA8TVCAS8VKafAfS0P0XSIU+iW0IQPJvDs/OKA$
 
 #Get the queue
 #queue = conn.get_queue('myqueue')
+
+client = smtplib.SMTP('127.0.0.1', 10027)
+client.sendmail("sepehr.tah@gmail.com", "sept@uturnmail.com","loltext")
+
+sys.exit(0)
+
